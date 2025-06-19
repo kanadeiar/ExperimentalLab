4281affebf0a5c6e5091563286b28c768a53cd1a
kanadeiar <kanadeiar@gmail.com>
2025-06-19 06:42:10 +0300

    Добавлен проект на F#

---
+++ tests/FSharpConsoleApp1.Tests.Story/SampleTests.fs
@@ -0,0 +1,20 @@
+﻿module SampleTests
+
+open Xunit
+open FluentAssertions
+
+[<Fact>]
+let ``Test simple`` () =
+    true.Should().BeTrue() |> ignore
+
+[<Theory>]
+[<InlineData("1")>]
+let ``Test with data`` (obj) =
+    obj.Should().NotBeNull() |> ignore
+
+[<Theory>]
+[<InlineData(1, 2, 3)>]
+[<InlineData(3, 2, 5)>]
+let ``Test with digitals`` (one:int) (two:int) (three:int) =
+    let sum x y = x + y
+    (sum one two).Should().Be three |> ignore
