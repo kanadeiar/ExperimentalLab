463af2e41d343d599f36bb7a997f857f4c33d67a
kanadeiar <kanadeiar@gmail.com>
2025-06-19 06:14:09 +0300

    Первые проекты

---
+++ tests/ConsoleApp1.Tests.Story/DeveloperTests.cs
@@ -0,0 +1,12 @@
+﻿using FluentAssertions;
+
+namespace ConsoleApp1.Tests.Story;
+
+public class DeveloperTests
+{
+    [Fact(DisplayName = "Тест разработчика")]
+    public void Test1()
+    {
+        true.Should().BeTrue();
+    }
+}
---
+++ tests/FrameworkConsoleApp1.Tests.Story/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace FrameworkConsoleApp1.Tests.Story
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
